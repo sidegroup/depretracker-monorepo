2f08213052476dd7bb49da93f1ccfc73845065f7
root <root@LAPTOP-T162DE81>
2024-07-03 21:56:51 -0300

    melhorias

--- side-web/app/controllers/line_translations_controller.rb
+++ side-web/app/controllers/line_translations_controller.rb
@@ -2,8 +2,13 @@ class LineTranslationsController < ApplicationController
   before_action :set_line_translation, only: %i[ show edit update destroy ]
 
   def index
-    @pending_line_translations = LineTranslation.not_reviewed.first(5)
+    @pending_line_translations = LineTranslation.not_reviewed.page(params[:page]).per(5)
+    @reviewed_line_translations = LineTranslation.reviewed.page(params[:page]).per(5)
+  end
 
-    @reviewed_line_translations = LineTranslation.reviewed.last(5)
+  private
+
+  def set_line_translation
+    @line_translation = LineTranslation.find(params[:id])
   end
 end
