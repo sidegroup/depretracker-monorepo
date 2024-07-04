0d9bef1081374d40f5b762e422e2db963a9b8779
Kelvin Romero <kelvinromero@gmail.com>
2024-07-04 20:59:22 -0300

    examples

--- side-web/app/controllers/line_translations_controller.rb
+++ side-web/app/controllers/line_translations_controller.rb
@@ -2,6 +2,13 @@ class LineTranslationsController < ApplicationController
   before_action :set_line_translation, only: %i[ show edit update destroy ]
 
   def index
+
+    @lines_pending_review_by_file = []
+    FileTranslation.all.each do |f|
+      @lines_pending_review_by_file << f.line_translations.not_reviewed
+    end
+
+
     @pending_line_translations = LineTranslation.not_reviewed.first(5)
 
     @reviewed_line_translations = LineTranslation.reviewed.last(5)
