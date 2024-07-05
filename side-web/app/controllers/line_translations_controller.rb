b7380a51b4ed75b481f638e6a17619034969a370
root <root@LAPTOP-T162DE81>
2024-07-04 22:43:23 -0300

    alterações

--- side-web/app/controllers/line_translations_controller.rb
+++ side-web/app/controllers/line_translations_controller.rb
@@ -2,6 +2,17 @@ class LineTranslationsController < ApplicationController
   before_action :set_line_translation, only: %i[ show edit update destroy ]
 
   def index
+
+    @lines_pending_review_by_file = Hash.new
+     FileTranslation.all.each do |f|
+       @lines_pending_review_by_file[f.original_file.name] = f.line_translations.not_reviewed.first(5)
+     end 
+
+     @lines_review_by_file = Hash.new
+     FileTranslation.all.each do |f|
+       @lines_review_by_file[f.original_file.name] = f.line_translations.reviewed.last(5)
+     end 
+
     @pending_line_translations = LineTranslation.not_reviewed.page(params[:page]).per(5)
     @reviewed_line_translations = LineTranslation.reviewed.page(params[:page]).per(5)
   end
