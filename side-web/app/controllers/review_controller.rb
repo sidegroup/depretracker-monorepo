15ec7e8e00f338dfe51440ad655e62e24778416d
Taw-Ham Almeida Balbino de Paula <taw.ham.balbino9914@gmail.com>
2024-07-09 20:58:23 -0300

    reduzindo quantidade de requisições

--- side-web/app/controllers/review_controller.rb
+++ side-web/app/controllers/review_controller.rb
@@ -16,15 +16,20 @@ class ReviewController < ApplicationController
   def create
     if @line_translation.update(example_params)
       @line_translation.update!(reviewed: true)
+      file_translation = @line_translation.file_translation
+      if file_translation.line_translations.count == LineTranslation.reviewedFileTranslation(file_translation).count
+        file_translation.update!(status: FileTranslation.statuses[:'Completed'])
+
+      end
+       # == file_translation.line_translations.count ? file_translation.update!(status: FileTranslation.statuses[:'Completed']) : file_translation.update!(status: FileTranslation.statuses[:'In Progress'])
       @next_line = LineTranslation.not_reviewed.first
       flash[:notice] = "Tradução revisada com sucesso."
     else
       flash[:notice] = "Erro ao revisar tradução."
     end
-
+    # @line_translation.file_translation.update!(status: FileTranslation.statuses[:'Completed'])
     if @next_line.nil?
       flash[:notice] = "Todos as traduções foram revisados."
-      @line_translation.file_translation.update!(status: FileTranslation.statuses[:'Completed'])
       redirect_to line_translations_path
       return
     end
